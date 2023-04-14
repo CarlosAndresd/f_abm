ccb4e3906ae2459f3090ce661754e58de6dafffc
Carlos Andres <CarlosAndresd@users.noreply.github.comm>
2023-04-14 13:53:22 +0200

    Add the src/code_text.py script to test the code when it is downloaded in another computer

---
+++ src/code_test.py
@@ -0,0 +1,16 @@
+from basiccreation import *
+from plot_functions import *
+
+
+opinions = a_random_initial_opinion_distribution(num_agents=10)
+
+grid = np.array([[x, y] for x in np.linspace(0, 1, 5) for y in np.linspace(-1, 1, 5)  # 11 and 21
+				if (((y-x) < 0.000001) and ((y+x) > -0.000001))]).round(decimals=3)
+
+_ = create_many_opinions(num_agents=100, file_name='standard_initial_opinions', grid=grid, show_result=False)
+
+fig = plt.figure(figsize=(10, 7))
+ax1 = fig.add_subplot(111)
+plot_histogram(ax1, opinions)
+plt.gcf().canvas.draw()
+
